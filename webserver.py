3f6dfb65cea6d312f302e8b8f1d20a5e7ee5f4f7
Firmin L <45711398+Firmin-Launay@users.noreply.github.com>
2021-03-03 20:14:03 +0100

    Update web server

--- webserver.py
+++ webserver.py
@@ -6,28 +6,29 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    if request.url_root == "https://docs.raspimote.tk":
+    print(request.url_root)
+    if "docs.raspimote.tk" in request.url_root:
         return send_file("index.html")
     else:
         return "<h1>Nothing here</h1>", 404
 
 @app.route('/css/<path:path>')
 def css(path):
-    if request.url_root == "https://docs.raspimote.tk":
+    if "docs.raspimote.tk" in request.url_root:
         return send_from_directory('css', path)
     else:
         return "<h1>Nothing here</h1>", 404
 
 @app.route('/img/<path:path>')
 def img(path):
-    if request.url_root == "https://docs.raspimote.tk":
+    if "docs.raspimote.tk" in request.url_root:
         return send_from_directory('img', path)
     else:
         return "<h1>Nothing here</h1>", 404
 
 @app.route('/js/<path:path>')
 def js(path):
-    if request.url_root == "https://docs.raspimote.tk":
+    if "docs.raspimote.tk" in request.url_root:
         return send_from_directory('js', path)
     else:
         return "<h1>Nothing here</h1>", 404
@@ -36,7 +37,7 @@ def js(path):
 
 
 my_app = WSGIPathInfoDispatcher({'/': app})
-server = WSGIServer(('0.0.0.0', 9876), my_app)
+server = WSGIServer(('0.0.0.0', 4430), my_app)
 
 ssl_cert = "cert.pem"
 ssl_key = "key.key"
