46015679c6bc72fe182c4a81f033dfedfe4830d2
Firmin L <45711398+Firmin-Launay@users.noreply.github.com>
2021-03-04 09:49:53 +0100

    Improve website (GitHub API)

--- js/js_root.js
+++ js/js_root.js
@@ -3,6 +3,20 @@ window.onload = init;
 function init() {
     var year = new Date().getFullYear();
     document.getElementById("footer").innerHTML = year + ", RaspiMote.";
+    let url = 'https://api.github.com/repos/RaspiMote/RaspiMote/releases';
+    fetch(url).then(res => res.json()).then((out) => {
+        if (out[0] != undefined) {
+            var version = out[0]["tag_name"];
+            var release_url = out[0]["html_url"];
+            document.getElementById("version").innerHTML = "The current version of RaspiMote is " + version + ". Download it <a target=\"_blank\" href=\"" + release_url + "\">here</a>."
+        }
+        else {
+            document.getElementById("version").innerHTML = "This project is currently in development. Consequently, no release is available for the moment."
+        }
+    })
+    .catch(err => {
+        throw err
+    });
     $(".fade").fadeOut(1500);
 }
 
