4e24807431abe0633bd6456acc9a40a6234e98ec
Firmin L <45711398+Firmin-Launay@users.noreply.github.com>
2021-03-04 07:46:23 +0100

    Fixed bug (buttons didn't work on Safari iOS)

--- js/js_root.js
+++ js/js_root.js
@@ -6,13 +6,35 @@ function init() {
     $(".fade").fadeOut(1500);
 }
 
+function windowLocation(url){
+    var X = setTimeout(function(){
+        window.location.replace(url);
+        return true;
+    },300);
+
+    if( window.location = url ){
+        clearTimeout(X);
+        return true;
+    } else {
+        if( window.location.href = url ){
+            clearTimeout(X);
+            return true;
+        }else{
+            clearTimeout(X);
+            window.location.replace(url);
+            return true;
+        }
+    }
+    return false;
+};
+
 function toDocs() {
     var url = "https://docs.raspimote.tk/";
     if (KeyPressing.isKeyPressed(17) || KeyPressing.isKeyPressed(16)) {
         window.open(url);
     }
     else {
-        window.location.href = url;
+        windowLocation(url);
     }
 }
 
@@ -22,7 +44,7 @@ function toGitHub() {
         window.open(url);
     }
     else {
-        window.location.href = url;
+        windowLocation(url);
     }
 }
 
